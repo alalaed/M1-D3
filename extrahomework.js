8c7d411e376ea634acad7d3b79a3af267a553511
Ali Alaeddine <a.alaeddine@live.com>
2021-12-05 18:33:02 +0100

    ex.6 solved

--- extrahomework.js
+++ extrahomework.js
@@ -71,8 +71,12 @@ console.log (average)
  Write a piece of code for finding the longest of two given strings.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let string1 = "is this string longer?"
+let string2 = "or is this string longer?"
+let longestString = " "
+string1.length > string2.length ? longestString=string1 : longestString=string2
 
+console.log(longestString)
 /* EXTRA 7
  Write a piece of code for checking if a given value is a integer or not.
 */
---
+++ trial.js
@@ -0,0 +1,6 @@
+let string1 = "is this string longer?"
+let string2 = "or is this string longer?"
+let longestString = " "
+string1.length > string2.length ? longestString=string1 : longestString=string2
+
+console.log(longestString)
